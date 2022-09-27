65b58ba04b04622f73e7470a70614f66173ba972
Arogya Saakha <aros22@hampshire.edu>
2022-09-26 20:32:01 -0400

    Version 1

--- Conversation.java
+++ Conversation.java
@@ -1,6 +1,70 @@
+import java.util.Scanner;
 class Conversation {
 
+  /**
+   * @param arguments
+   */
   public static void main(String[] arguments) {
-    // You will start the conversation here.
+    Scanner keyboard = new Scanner(System.in); //created a scanner to get input from the user
+    Scanner in = new Scanner(System.in); 
+
+    String reply;
+    int a;
+    System.out.print("How many round?");
+    int i = keyboard.nextInt();
+    a = i;
+    while(i !=0) //loop function to excute number of rounds for the conversation
+      {
+        if (a == i)
+          {
+            System.out.println("Hi there!  What's on your mind?");
+            reply = in.nextLine();
+            String word = reply.replace("I", "You");
+            String word_1 = word.replace("me", "you");
+            String word_2 = word_1.replace("Me", "You");
+            String word_3 = word_2.replace("am", "are");
+            String word_4 = word_3.replace("'m", "'re");
+            String word_5 = word_4.replace("your", "my");
+            String word_6 = word_5.replace("Your", "My");
+            String word_7 = word_6.replace("you", "I");
+            String word_8 = word_7.replace("You", "I");
+            String word_9 = word_8.replace("my", "your");
+            String word_10 = word_9.replace("My", "Your");//replaces and mirrors some words. Has errors. Need to use loop function
+            
+            System.out.println(word_10 +"?");
+          }
+        i --;  
+        if(i==1)
+          {
+            reply = in.nextLine();
+            System.out.println("Mmm-hm.");
+            System.out.println("Cya!");
+          }
+        else
+          {
+            reply = in.nextLine();
+            String word = reply.replace("I", "You");
+            String word_1 = word.replace("me", "you");
+            String word_2 = word_1.replace("Me", "You");
+            String word_3 = word_2.replace("am", "are");
+            String word_4 = word_3.replace("'m", "'re");
+            String word_5 = word_4.replace("your", "my");
+            String word_6 = word_5.replace("Your", "My");
+            String word_7 = word_6.replace("you", "I");
+            String word_8 = word_7.replace("You", "I");
+            String word_9 = word_8.replace("my", "your");
+            String word_10 = word_9.replace("My", "Your");//replaces and mirrors some words. Has errors. Need to use loop function
+            System.out.println(word_10 +"?");
+          }
+
+      } 
+     
+
+
+      
+
+
   }
+
 }
+
--- reflection.md
+++ reflection.md
@@ -2,6 +2,9 @@ Use this file to record your reflection on this assignment.
 
 - Which classmates did you consult with while working on this assignment?
 - Which session(s) of TA / office hours did you attend?
+  I could not attend the TA hours due to clash in my schedule and commuting issues.
 - What are your initial impressions of Java? 
+  I used to program in java before by creating a GUI first and then coding parts of it. Directly coding into a bank sheet has been a new experience for me but I am finding it interesting. Java seems more complicated than python as you need define all the variables.
 - Can you draw any conclusion about programming in general from the similarities or the differences between the two languages? 
+  Python is a interpeter while java is a compiler. I personally find java more organized than python and easier to use as I'm more used to it. Both language has their own use and are generally better to use for different purposes.
 - What worked, what didn't, what advice would you give someone taking this course in the future?
